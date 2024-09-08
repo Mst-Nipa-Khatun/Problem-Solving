baab789d192b20c9199ea9e644f10da60e830af9
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-09-08 18:57:46 +0600

    Add files via upload

---
+++ CollectionsPb1.java
@@ -0,0 +1,17 @@
+package com.mycompany.problemsolving;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectionsPb1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        String[] names={"Nipa","Kibria","Anika","Eva"};
+        Collections.addAll(list,names);
+        Collections.sort(list);
+        System.out.println(list);
+        
+    }
+}
