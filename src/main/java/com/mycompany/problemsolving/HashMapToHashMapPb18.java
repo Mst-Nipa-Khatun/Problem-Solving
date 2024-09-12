047aea3b16b20d74b01b85c040a56b0fe125a308
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-09-12 17:32:47 +0600

    Add files via upload

--- src/main/java/com/mycompany/problemsolving/HashMapToHashMapPb18.java
+++ src/main/java/com/mycompany/problemsolving/HashMapToHashMapPb18.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 
 public class HashMapToHashMapPb18 {
     public static void main(String[] args) {
-        HashMap<String, Integer> values = new HashMap< >();
-        values.put("a", 1);
-        values.put("b", 2);
-        values.put("c", 3);
-        System.out.println(values);
+        HashMap<String, Integer> HashMap1 = new HashMap< >();
+        HashMap1.put("a", 1);
+        HashMap1.put("b", 2);
+        HashMap1.put("c", 3);
+        System.out.println(HashMap1);
 
-        HashMap<String, Integer> values2 = new HashMap< >();
+        HashMap<String, Integer> HashMap2 = new HashMap< >();
+        for (String key : HashMap1.keySet()) {
+            HashMap2.put(key, HashMap1.get(key));
+
+        }
+        System.out.println(HashMap2);
 
 
 
