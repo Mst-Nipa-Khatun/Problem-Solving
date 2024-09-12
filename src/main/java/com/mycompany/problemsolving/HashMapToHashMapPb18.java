3a6a680fcf6310e7e2f03df200b0bbad7d84e5dc
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-09-12 16:05:59 +0600

    Add files via upload

---
+++ src/main/java/com/mycompany/problemsolving/HashMapToHashMapPb18.java
@@ -0,0 +1,20 @@
+package com.mycompany.problemsolving;
+
+import java.util.HashMap;
+
+public class HashMapToHashMapPb18 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> values = new HashMap< >();
+        values.put("a", 1);
+        values.put("b", 2);
+        values.put("c", 3);
+        System.out.println(values);
+
+        HashMap<String, Integer> values2 = new HashMap< >();
+
+
+
+
+
+    }
+}
