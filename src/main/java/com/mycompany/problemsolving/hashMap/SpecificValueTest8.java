fda28f1fb5ffd1d145592b464b1b688f1f17b2b2
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-15 16:14:34 +0600

    HashMap Problem Solving

---
+++ src/main/java/com/mycompany/problemsolving/hashMap/SpecificValueTest8.java
@@ -0,0 +1,21 @@
+package com.mycompany.problemsolving.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpecificValueTest8 {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        System.out.println(map);
+
+        boolean b= map.containsValue("two");
+        System.out.println(b);
+
+        boolean b1= map.containsValue("six");
+        System.out.println(b1);
+    }
+}
