5dce2570b980477c2780f0750180637b75244cfa
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-15 14:48:09 +0600

    Basic Test

--- src/main/java/com/mycompany/problemsolving/ArrayToHashSetPb23.java
+++ src/main/java/com/mycompany/problemsolving/ArrayToHashSetPb23.java
@@ -7,7 +7,8 @@ import java.util.Set;
 public class ArrayToHashSetPb23 {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
-       // System.out.println("Arrays:" +arr);
+        System.out.println(Arrays.toString(arr));
+
 
         Set<Integer> hashSet = new HashSet<Integer>(arr.length);
         for(int i : arr) {
