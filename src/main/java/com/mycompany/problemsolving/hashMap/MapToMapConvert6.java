cfeeb38d2ea00dad57219a42128e1a943749f0f6
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-15 15:58:46 +0600

    HashMap Problem Solving

---
+++ src/main/java/com/mycompany/problemsolving/hashMap/MapToMapConvert6.java
@@ -0,0 +1,17 @@
+package com.mycompany.problemsolving.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapToMapConvert6 {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        System.out.println(map);
+         Map<Integer, String> map2 = map;
+         System.out.println(map2);
+    }
+}
