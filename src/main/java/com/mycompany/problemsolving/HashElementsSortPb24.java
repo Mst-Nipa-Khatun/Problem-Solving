9f8882805b0fe5f6b15833af240ed064835268d0
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-15 14:52:18 +0600

    Problem Solving

---
+++ src/main/java/com/mycompany/problemsolving/HashElementsSortPb24.java
@@ -0,0 +1,16 @@
+package com.mycompany.problemsolving;
+
+import java.util.HashSet;
+
+public class HashElementsSortPb24 {
+    public static void main(String[] args) {
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("B");
+        hashSet.add("C");
+        hashSet.add("D");
+        hashSet.add("A");
+        hashSet.add("Z");
+        hashSet.add("null");
+        System.out.println(hashSet);
+    }
+}
