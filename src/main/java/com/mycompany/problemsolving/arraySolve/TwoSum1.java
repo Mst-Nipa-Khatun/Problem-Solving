9095eff54f9582202551ae53af1cd429cde04019
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-17 14:47:10 +0600

    HashMap Problem Solving

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/TwoSum1.java
@@ -0,0 +1,24 @@
+package com.mycompany.problemsolving.arraySolve;
+
+import java.util.Arrays;
+
+public class TwoSum1 {
+    public static void main(String[] args) {
+        int[] a = {2, 7, 11, 15};
+        int[]a2 =new int[2];
+        int target = 13;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] + a[j] == target) {
+                    a2[0] = i;
+                    a2[1] = j;
+                    //System.out.println(a[i] + " " + a[j]);
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(a2));
+
+    }
+
+}
