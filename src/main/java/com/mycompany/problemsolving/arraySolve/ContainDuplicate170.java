7f0169e3ce16c6734cddf8964d95f8f216eadb25
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-18 17:27:24 +0600

    Arrays Problem Solving

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/ContainDuplicate170.java
@@ -0,0 +1,19 @@
+package com.mycompany.problemsolving.arraySolve;
+
+import java.util.Arrays;
+
+public class ContainDuplicate170 {
+public static void main(String[] args) {
+    int[] arr={1,2,3,4,5};
+    int[] arr2=new int[arr.length];
+    Arrays.sort(arr);
+
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i] == arr[i-1]) {
+            System.out.println(true);
+            return;
+        }
+    }
+    System.out.println(false);
+}
+}
