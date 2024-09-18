3bd233c0b6603b4740e9f0da09c7eb2b449cd62b
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-18 17:29:23 +0600

    Arrays Problem Solving

--- src/main/java/com/mycompany/problemsolving/arraySolve/ContainDuplicate170.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/ContainDuplicate170.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ContainDuplicate170 {
 public static void main(String[] args) {
-    int[] arr={1,2,3,4,5};
+    int[] arr={1,2,3,4,1};
     int[] arr2=new int[arr.length];
     Arrays.sort(arr);
 
