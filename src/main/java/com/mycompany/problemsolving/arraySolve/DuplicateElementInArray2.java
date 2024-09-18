2d28125f80aaeb90d4c26a678e5ebb5afb7ff3da
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-18 14:46:47 +0600

    Arrays Problem Solving

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/DuplicateElementInArray2.java
@@ -0,0 +1,25 @@
+package com.mycompany.problemsolving.arraySolve;
+
+public class DuplicateElementInArray2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 5, 6, 7, 7, 9, 10, 10};
+        int[] arr2 = new int[arr.length];
+        int k = 1;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(" " + arr[j]);
+
+//                    if (arr[i] != arr[i - 1]) {
+//                        arr[k] = arr[i];
+//                        k++;
+
+
+                }
+
+            }
+
+        }
+
+    }}
+
