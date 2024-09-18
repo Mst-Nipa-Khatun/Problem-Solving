8195226d82c75b53f266000d5ebea1f198e76888
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-18 17:21:01 +0600

    Arrays Problem Solving

--- src/main/java/com/mycompany/problemsolving/arraySolve/SingleNumber136.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/SingleNumber136.java
@@ -2,7 +2,6 @@ package com.mycompany.problemsolving.arraySolve;
 
 public class SingleNumber136 {
     public static void main(String[] args) {
-        SingleNumber136 sn = new SingleNumber136();
         int[] nums = {4,1,2,1,2};
         int val = 0;
 
