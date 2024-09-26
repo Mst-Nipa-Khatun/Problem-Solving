e467615cbd64c556de18e9d4fd805bc97b9c6eae
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-26 17:02:00 +0600

    Arrays Problem Solving

--- src/main/java/com/mycompany/problemsolving/arraySolve/MaximumSubarray643.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/MaximumSubarray643.java
@@ -4,17 +4,15 @@ public class MaximumSubarray643 {
     public static void main(String[] args) {
         int[] nums = {1,12,-5,-6,50,3};
         int k=4;
-        int currentSum = 0;
+        int sum = 0;
         for (int i = 0; i < k; ++i) {
-            currentSum += nums[i];
+            sum += nums[i];
         }
-
-        int maxSum = currentSum;
+        int maxSum = sum;
 
         for (int i = k; i < nums.length; ++i) {
-            currentSum += (nums[i] - nums[i - k]);
-
-            maxSum = Math.max(maxSum, currentSum);
+            sum += (nums[i] - nums[i - k]);
+            maxSum = Math.max(maxSum, sum);
         }
         double v = maxSum * 1.0 / k;
         System.out.println(v);
