01adadc81ddad1495c7e03eeac73d9ce99e0b7fc
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-26 15:39:57 +0600

    Arrays Problem Solving

--- src/main/java/com/mycompany/problemsolving/arraySolve/MinimumIndexSum599.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/MinimumIndexSum599.java
@@ -2,16 +2,15 @@ package com.mycompany.problemsolving.arraySolve;
 
 public class MinimumIndexSum599 {
     public static void main(String[] args) {
-        String[] list1 = {"happy","sad","good","bad"};
-        String[] list2 = {"sad","happy","good","bad"};
+        String[] list1 = {"happy", "sad", "good", "bad"};
+        String[] list2 = {"sad", "happy", "good", "bad"};
         String[] list3 = new String[]{};
-
-         for (int i = 0; i < list1.length; i++)
-         {
-             if(list1[i].equals(list2[i]))
-             {
-                 System.out.println(list1[i]);
-             }
-         }
+        String val = "null";
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i].equals(list2[i])) {
+                val = list1[i]+list2[i];
+                System.out.println(list1[i]);
+            }
+        }
     }
 }
