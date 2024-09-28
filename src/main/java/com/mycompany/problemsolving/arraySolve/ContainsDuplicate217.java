e013fb4639ee3ffb8b81c6b01588ad591e854251
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-28 23:04:45 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/ContainsDuplicate217.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/ContainsDuplicate217.java
@@ -1,19 +1,34 @@
 package com.mycompany.problemsolving.arraySolve;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ContainsDuplicate217 {
-public static void main(String[] args) {
-    int[] arr={1,2,3,4,1};
-    int[] arr2=new int[arr.length];
-    Arrays.sort(arr);
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 1};
+        int[] arr2 = new int[arr.length];
+        Arrays.sort(arr);
 
-    for (int i = 1; i < arr.length; i++) {
-        if (arr[i] == arr[i-1]) {
-            System.out.println(true);
-            return;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                System.out.println(true);
+                return;
+            }
         }
+        System.out.println(false);
     }
-    System.out.println(false);
-}
+
+    public static boolean containsDuplicate(int[] nums) {
+        //for leetCode
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
 }
