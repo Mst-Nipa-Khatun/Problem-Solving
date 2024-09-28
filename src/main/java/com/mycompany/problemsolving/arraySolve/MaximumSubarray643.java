b8f0912247017b1c8047bcae4d263df7bd162888
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-28 22:39:04 +0600

    solve MaximumSubarray leetCode  643

--- src/main/java/com/mycompany/problemsolving/arraySolve/MaximumSubarray643.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/MaximumSubarray643.java
@@ -2,19 +2,37 @@ package com.mycompany.problemsolving.arraySolve;
 
 public class MaximumSubarray643 {
     public static void main(String[] args) {
-        int[] nums = {1,12,-5,-6,50,3};
-        int k=4;
-        int sum = 0;
-        for (int i = 0; i < k; ++i) {
-            sum += nums[i];
-        }
-        int maxSum = sum;
-
-        for (int i = k; i < nums.length; ++i) {
-            sum += (nums[i] - nums[i - k]);
-            maxSum = Math.max(maxSum, sum);
-        }
-        double v = maxSum * 1.0 / k;
-        System.out.println(v);
+        int[] nums = {-1};
+        int k = 1;
+        /*
+            int[] nums = {5};
+            int k=1;
+         */
+        System.out.printf("" + findMaxAverage(nums, k));
     }
+
+    public static double findMaxAverage(int[] nums, int k) {
+        int length = nums.length;
+        double maxSum = 0;
+        for (int i = 0; i < length; i++) {
+            int expected = i + (k - 1);
+            if (expected > length - 1)
+                break;
+            int subSum = 0;
+            for (int j = i; j <= expected; j++)
+                subSum += nums[j];
+            if (subSum<0 && maxSum==0){
+                maxSum = subSum;
+                continue;
+            }
+            maxSum = getMaxNumber(maxSum, subSum);
+        }
+        return maxSum / k;
+    }
+
+    public static double getMaxNumber(double a, double b) {
+        if (a > b) return a;//we can also do this logic using Math.max() function.
+        return b;
+    }
+
 }
