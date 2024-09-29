0f4211d74e8bd7689e98b27032644049e6556129
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-29 15:23:39 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/ContainsDuplicate217.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/ContainsDuplicate217.java
@@ -7,11 +7,20 @@ import java.util.Map;
 public class ContainsDuplicate217 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 1};
-        System.out.println(containsDuplicate(arr));
+        int[] arr2 = new int[arr.length];
+        Arrays.sort(arr);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                System.out.println(true);
+                return;
+            }
+        }
+        System.out.println(false);
     }
 
     public static boolean containsDuplicate(int[] nums) {
-        //leetCode accepted solution
+        //for leetCode
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.get(nums[i]) != null) {
