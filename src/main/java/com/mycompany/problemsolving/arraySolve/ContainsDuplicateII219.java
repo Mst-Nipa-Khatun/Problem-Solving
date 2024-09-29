9fbaafe80dbf5846b777cdf1526445d9f3c7447b
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-29 10:55:44 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/ContainsDuplicateII219.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/ContainsDuplicateII219.java
@@ -1,23 +1,27 @@
 package com.mycompany.problemsolving.arraySolve;
 
 
-public class ContainsDuplicateII219 {
-    public static void main(String[] args) {
-        //int[] nums = {1,2,3,1,2,3};
-        int[] nums = {1,2,3,1};
-        int k = 3;
-        //int k=2;
-        for (int i = 0; i < nums.length; i++) { //i=0; nums[0]=1
-            for (int j = i + 1; j <= i + k && j < nums.length; j++) {// j=1,nums[1]=2 No match;
-                // j=2,nums[2]=3,no match;j=3,nums[3]=1;match
-                // because i from nums[0]=1;so(abs(3 - 0) = 3, which satisfies <= k)
-                if (nums[i] == nums[j]) {//nums[0]=nums[3] match true
-                    System.out.println(true);
-                    return;
-                }
-            }
-        }
-        System.out.println(false);
+import java.util.HashMap;
+import java.util.Map;
 
+public class ContainsDuplicateII219 {
+
+    public static void main(String[] args) {
+        int[] nums = {1,0,1,1};
+        System.out.println(containsNearbyDuplicate(nums, 1));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        //leetCode accepted solution
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer value = map.get(nums[i]);
+            if (value != null) {
+               if ((i - value) <= k)
+                   return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
     }
 }
