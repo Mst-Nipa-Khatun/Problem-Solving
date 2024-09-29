bed3363aa8eaf1df6452368e5742488cd2a8c455
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-09-29 15:23:22 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/MajorityElements169.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/MajorityElements169.java
@@ -2,18 +2,13 @@ package com.mycompany.problemsolving.arraySolve;
 
 public class MajorityElements169 {
     public static void main(String[] args) {
+
+        //@INFO hoi nai eta bhalo kore pb dekho
+
         int[] nums1 = {2, 2, 1, 1, 1, 2, 2};
         int[] arr2 = new int[nums1.length];
 
         int count = 0, temp = 0;
-
-//        for(int i = 1; i < nums1.length; i++){
-//            for(int j = i+1; j < nums1.length; j++){
-//                if(nums1[i] == nums1[j]){
-//                    System.out.println("Num"+nums1[i]);
-//                }
-//            }
-//        }
 
         for (int num : nums1) {
             if (count == 0) {
