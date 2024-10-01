fcc6251214b121b79d9104df1d15ed57f9c9d111
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-01 12:28:43 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/LongestContineous674.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/LongestContineous674.java
@@ -1,23 +1,37 @@
 package com.mycompany.problemsolving.arraySolve;
 
+import java.util.*;
+
 public class LongestContineous674 {
     public static void main(String[] args) {
-        int[] nums = {1, 3, 5, 4, 7};
-        int count = 0;
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < max) {
-                break;
-            } else {
-                max = nums[i];
-                count++;
-            }
-            System.out.print(" "+max);
-        }
+       // int[] nums = {3,0,6,2,4,7,0,0};
+        int[] nums = {1,3,5,7};
+        System.out.println(""+findLengthOfLCIS(nums));
     }
 
-//    public static int longestContinuous(int a,int b ) {
-//        if(a>b) return a;
-//        return b;
+    public static int findLengthOfLCIS(int[] nums) {
+        int count = 0;
+        int max = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int current  = nums[i];
+            if (current <= max) {
+                list.add(count);
+                max = current;
+                count = 1;
+            } else {
+                max = current;
+                count++;
+            }
+        }
+        //below 2 line for sort array and arrayList
+        //for array Arrays.sort();
+        //for list  Collections.sort();
+        Collections.sort(list, Collections.reverseOrder());
+        if (list.isEmpty()) {
+            return Math.max(count, 0);
+        }
+        return Math.max(count, list.get(0));
+    }
 
 }
