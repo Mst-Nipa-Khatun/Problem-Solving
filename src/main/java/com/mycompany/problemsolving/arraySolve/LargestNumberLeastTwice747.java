9769d6b20cb43193be85b1ca961ae3d8ecbec0e9
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-02 16:58:37 +0600

    solve ContainsDuplicate217 leetCode  217

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/LargestNumberLeastTwice747.java
@@ -0,0 +1,41 @@
+package com.mycompany.problemsolving.arraySolve;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LargestNumberLeastTwice747 {
+    public static void main(String[] args) {
+        //int nums declare
+        //full array theke findout kora max konta
+        //sei max k full array te j value gulo ase double hoy kina check kora
+        //double hole return 1 otherwise return -1.
+
+        //int[] nums = {3,6,1,0};
+        int[] nums = {3,4,1,7};
+        int max=0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>max){
+                //max=nums[i]*2;
+                max=nums[i];
+            }else if(max==nums[i]*2){
+                max=nums[i];
+                //return 1;
+            }else {
+                //return -1;
+            }
+
+        }
+        System.out.println(max);
+
+
+//        List<Integer> values = new ArrayList<>();
+//        for (int num : nums) {
+//            if (num => nums[k]) {
+//                 values.add(num);
+//                 System.out.println(num);
+//            }
+
+        }
+    }
+
