f5fb5de01c03fb654638db1323912f7736b6e52c
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-03 12:53:12 +0600

    solve ContainsDuplicate217 leetCode  217

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/BinarySearch704.java
@@ -0,0 +1,30 @@
+package com.mycompany.problemsolving.arraySolve;
+
+public class BinarySearch704 {
+    public static void main(String[] args) {
+        //int array declare
+        //target value in binary method
+        //loop check if value in this array print index number
+        //if not found number in this array return -1.
+
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+      //  System.out.println(nums[left] == target ? left : -1);
+//            if (nums[left] == target) {
+//                System.out.println(nums[left]);
+//            } else {
+//                System.out.println(-1);
+//           }
+        }
+    }
