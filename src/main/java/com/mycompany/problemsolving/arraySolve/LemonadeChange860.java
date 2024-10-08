f26250281aa3a2ff9011be019eebb5cb6c26f3e8
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-08 13:59:24 +0600

    solve ContainsDuplicate217 leetCode  217

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/LemonadeChange860.java
@@ -0,0 +1,29 @@
+package com.mycompany.problemsolving.arraySolve;
+
+public class LemonadeChange860 {
+    public static void main(String[] args) {
+        int[] bills = {5,5,5,10,20};
+        int fives = 0;
+        int tens = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                fives--;
+                tens++;
+            } else {  // bill == 20
+                if (tens > 0) {
+                    tens--;
+                    fives--;
+                } else {
+                    fives =fives- 3;
+                }
+            }
+            if (fives < 0) {
+                System.out.println(false);
+            }
+        }
+        System.out.println(true);
+    }
+}
