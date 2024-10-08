cc6a7d57bdb9e967c0381103c5a04d03d2d0ec0d
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-08 11:38:20 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/NumberOfLines806.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/NumberOfLines806.java
@@ -7,10 +7,12 @@ public class NumberOfLines806 {
         int line=0;
         int last=1;
         int width=100;
+        int count=0;
        // char c : s.toCharArray()
         for(int i=0;i<s.length;i++)
         {
             if(widths[i]>=100){
+                //count=widths[i]+s;
                 width=widths[i];
                 System.out.println(width);
 
