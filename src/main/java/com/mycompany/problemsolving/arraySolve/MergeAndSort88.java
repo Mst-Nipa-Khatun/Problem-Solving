5580d50c70db7077378ce897560340ed4963c914
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-08 11:38:08 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/MergeAndSort88.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/MergeAndSort88.java
@@ -9,21 +9,6 @@ public class MergeAndSort88 {
 
         merge2(arr, 3, arr2, 3);
 
-        /*   int[] arr3 = new int[arr.length + arr2.length];
-        int k = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                while (i < arr.length && j < arr2.length) {
-                    arr3[k++] = arr[i++];
-                    arr3[k++] = arr2[j++];
-
-                }
-            }
-        }
-        Arrays.sort(arr3);
-        System.out.println(Arrays.toString(arr3));*/
-
     }
 
     public static void merge2(int[] nums1, int m, int[] nums2, int n) {
