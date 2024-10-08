a1b02925d2bc8b0384af48d08b52450a8799590a
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-08 16:38:08 +0600

    solve ContainsDuplicate217 leetCode  217

---
+++ src/main/java/com/mycompany/problemsolving/arraySolve/Transposematrix867.java
@@ -0,0 +1,16 @@
+package com.mycompany.problemsolving.arraySolve;
+
+public class Transposematrix867 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] transpose = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transpose[i][j] = matrix[j][i];
+                System.out.print(transpose[i][j] + " ");
+                // System.out.print("\n");
+            }
+            System.out.println(" ");
+        }
+    }
+}
