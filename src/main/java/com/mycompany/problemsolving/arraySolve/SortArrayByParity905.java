9b41f494e6252c574a34264e89ce6f7590e6093a
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-09 16:08:07 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/SortArrayByParity905.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/SortArrayByParity905.java
@@ -4,19 +4,18 @@ public class SortArrayByParity905 {
     public static void main(String[] args) {
         int[] nums = {3, 1, 2, 4};
         int[] nums2 = new int[nums.length];
-        int count = 0, k = 0;
+        int count = 0, even = 0, odd = nums.length - 1;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
-                k = nums[i];
-                System.out.print(k + " ");
-               count++;
-//            } else if (nums[i]% 2 != 0) {
-//                k = nums[i];
-//                System.out.print(k + " ");
+                nums2[even] = nums[i];
+                even++;
+               // System.out.println(nums2[even]);
+            } else {
+                nums2[odd] = nums[i];
+                odd--;
             }
         }
-       // System.out.print(count);
-
+        System.out.println(nums2);
     }
 }
