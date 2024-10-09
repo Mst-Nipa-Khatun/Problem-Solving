b322b6f2f9045323811d8210bf0acdce88e89ec0
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-10-09 15:04:40 +0600

    solve ContainsDuplicate217 leetCode  217

--- src/main/java/com/mycompany/problemsolving/arraySolve/MonotonicArray896.java
+++ src/main/java/com/mycompany/problemsolving/arraySolve/MonotonicArray896.java
@@ -3,7 +3,7 @@ package com.mycompany.problemsolving.arraySolve;
 public class MonotonicArray896 {
     public static void main(String[] args) {
         //int[] nums = {1, 2, 2,3,5, 4};
-        int[] nums={4,3,2,1,1,1};
+        int[] nums={4,3,2,0,1,1};
         //int[] nums2 = new int[nums.length];
 
         System.out.println(monotonictest(nums));
@@ -14,14 +14,14 @@ public class MonotonicArray896 {
         boolean decreasing = true;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < nums[i - 1]) {//2<(2-1)=2<1,4<(4-1)=4<3,3<(3-1)=3<2
-                increasing=false;
-                //return false;
+                increasing=false; //not increasing
             }
             if (nums[i] > nums[i - 1]) { //2>(2-1)=2>1,4>(4-1)=4>3,3>(3-1)=3>2
-                decreasing=false;
-               // return false;
+                decreasing=false; //not decreasing
             }
-
+            if (!increasing && !decreasing) {
+                return false;
+            }
         }
         return true;
     }
