b6ae06c50f513959f778e7f14a153000631a8f16
Mst-Nipa-Khatun <77069059+Mst-Nipa-Khatun@users.noreply.github.com>
2024-11-10 16:14:24 +0600

    Update Student.java

--- src/main/java/com/mycompany/problemsolving/customProjects/Student.java
+++ src/main/java/com/mycompany/problemsolving/customProjects/Student.java
@@ -10,11 +10,9 @@ public class Student {
     private String Address;
     private List<String> phoneNumbers;
 
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
